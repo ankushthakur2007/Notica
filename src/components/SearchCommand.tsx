aa825b375cba4cebcf8eb416ba3ea61da292f03d
Ankush Thakur <ankushthakur2007@gmail.com>
2025-07-29 16:50:29 +0530

    [dyad] Changed search shortcut to Cmd+O - wrote 1 file(s)

--- src/components/SearchCommand.tsx
+++ src/components/SearchCommand.tsx
@@ -27,7 +27,7 @@ export const SearchCommand = () => {
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
-      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+      if (e.key === "o" && (e.metaKey || e.ctrlKey)) {
         e.preventDefault();
         setIsOpen((open) => !open);
       }
